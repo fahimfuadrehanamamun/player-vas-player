1c179ae321fba514bc500de21cbe8c842429693f
Al-mamun <almamun.f.f.f@gmail.com>
2021-07-27 17:38:50 +0600

    guessing number player vs plyaer game's first version

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+let btn1=document.getElementById('p1btn')
+let btn2=document.getElementById('p2btn')
+let reset=document.getElementById('reset')
+let inputnumberp1=document.getElementById('inputnumberp1')
+let inputnumberp2=document.getElementById('inputnumberp2')
+let result=document.getElementById('result')
+
+console.log(btn1);
+console.log(btn2);
+console.log(reset);
+console.log(inputnumberp1);
+console.log(inputnumberp2);
+console.log(result);
+
+let x=Math.floor(Math.random()*10)+1;
+console.log(x);
+
+btn1.addEventListener('click',function winner1(){
+    let a=inputnumberp1.value;
+    if (a==x) {
+        result.innerHTML=(`Congratulation! player 1 is win the game.`)
+        // result.style.backgroundColor = 'green';
+
+    }else if(a==''){
+        alert('pls input something')
+    }
+    else{
+        result.innerHTML=(`Your guess is wrong`)
+        // result.style.backgroundColor = 'red';
+    }
+})
+btn2.addEventListener('click',function winner2(){
+    let b=inputnumberp2.value;
+    if (b==x) {
+        result.innerHTML=(`Congratulation! player 2 is win the game.`)
+        // result.style.backgroundColor = 'blue';
+    }else if(b==''){
+        alert('pls input something')
+    }
+    else{
+        result.innerHTML=(`Your guess is wrong`)
+        // result.style.backgroundColor = 'orange';    
+    }
+})
+function resetGame(){
+    result.classList.remove('winner1')
+    result.classList.remove('winner2')
+    result.innerText=`Result:`;
+    inputnumberp1.value=''
+    inputnumberp2.value=''
+}
+reset.addEventListener('click',resetGame)
+
+
+
